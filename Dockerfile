421ab8a4214baaccee9c34c1b00b7f0af0c617a6
Steve Zelek <smzelek@gmail.com>
2021-03-01 00:00:00 -0800

    add db initialization in docker

--- Dockerfile
+++ Dockerfile
@@ -2,8 +2,10 @@ FROM node:12-slim
 
 WORKDIR /usr/src/app
 
+COPY scripts .
+
 COPY package.json .
 COPY package-lock.json .
 
-CMD npm install
-# CMD npm run start:server
+RUN npm install
+CMD ./scripts/wait_for_it.sh mongodb:27017 --strict --timeout=300 && npm run initialize-db
--- db/init/init.ts
+++ db/init/init.ts
@@ -1,18 +1,18 @@
 import { connect } from "mongoose";
-import { IChatroom, IMessage, IUser, User, Chatroom, Message } from './schemas';
-import * as faker from 'faker';
+import { IChatroom, IMessage, IUser, User, Chatroom, Message } from '../schemas';
+import { internet, name, company, random, lorem } from 'faker';
 
 const default_users: IUser[] = Array.from({ length: 10 }, () => {
-    const firstname = faker.name.firstName();
-    const lastname = faker.name.lastName();
+    const firstname = name.firstName();
+    const lastname = name.lastName();
     return {
         name: `${firstname} ${lastname}`,
-        username: faker.internet.userName(firstname, lastname)
+        username: internet.userName(firstname, lastname)
     };
 });
 
 const default_chatrooms: IChatroom[] = Array.from({ length: 3 }, () => {
-    const name = `${faker.company.bsAdjective()} ${faker.company.bsNoun()}`
+    const name = `${company.bsAdjective()} ${company.bsNoun()}`
         .split(' ')
         .map(w => `${w[0].toUpperCase()}${w.substr(1)}`)
         .join(' ');
@@ -24,8 +24,8 @@ const default_chatrooms: IChatroom[] = Array.from({ length: 3 }, () => {
 });
 
 const default_messages: IMessage[] = Array.from({ length: 180 }, () => {
-    const content = faker.lorem.sentences(faker.random.number({ min: 0, max: 5 }))
-        || faker.lorem.words(1);
+    const content = lorem.sentences(random.number({ min: 0, max: 5 }))
+        || lorem.words(1);
 
     return {
         content,
@@ -35,7 +35,7 @@ const default_messages: IMessage[] = Array.from({ length: 180 }, () => {
 });
 
 const initializeDb = async function () {
-    await connect("mongodb://localhost:27017/talkieio", { useNewUrlParser: true, useUnifiedTopology: true });
+    await connect("mongodb://mongodb:27017/talkieio", { useNewUrlParser: true, useUnifiedTopology: true });
 
     await User.collection.insertMany(default_users);
     const users = await User.find({});
@@ -43,7 +43,7 @@ const initializeDb = async function () {
     await Chatroom.collection.insertMany(
         default_chatrooms.map((c): IChatroom => ({
             ...c,
-            user_createdby: faker.random.arrayElement(users).id
+            user_createdby: random.arrayElement(users).id
         }))
     );
     const chatrooms = await Chatroom.find({});
@@ -51,8 +51,8 @@ const initializeDb = async function () {
     await Message.collection.insertMany(
         default_messages.map((m): IMessage => ({
             ...m,
-            user_sentby: faker.random.arrayElement(users).id,
-            chatroom_sentto: faker.random.arrayElement(chatrooms).id
+            user_sentby: random.arrayElement(users).id,
+            chatroom_sentto: random.arrayElement(chatrooms).id
         }))
     );
 
