1db35013f34bf166128b3d6f5e6f72a9027e4c66
Steve Zelek <smzelek@gmail.com>
2021-03-02 16:03:24 -0800

    perf: added prod webpack config for server

---
+++ backend/webpack.common.js
@@ -0,0 +1,30 @@
+const path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const nodeExternals = require('webpack-node-externals')
+
+module.exports = {
+    entry: './backend/main.ts',
+    output: {
+        path: path.resolve('backend/dist'),
+        filename: 'server.js',
+    },
+    target: 'node',
+    externals: [nodeExternals()],
+    resolve: {
+        extensions: ['.ts', '.js'],
+    },
+    module: {
+        rules: [
+            {
+                test: /\.ts$/,
+                loaders: ['ts-loader'],
+                exclude: /node_modules/
+            },
+        ]
+    },
+    plugins: [
+        new CleanWebpackPlugin()
+    ],
+    mode: 'development',
+    devtool: 'inline-source-map',
+}
